8e7cbe6e36ae539d7c71a5418fa2bc2d368b5c23
codedbygunnaj <gunnajpchugh@gmail.com>
2025-10-06 18:38:51 +0000

    Recursion_Part2

--- medium/sequence.cpp
+++ medium/sequence.cpp
@@ -6,10 +6,34 @@
 //Better than recursion -> Power Sets (will see that later)
 #include<bits/stdc++.h>
 using namespace std;
-int subsets(vector<int>v){
-    
+void seq(vector<int> &nums,vector<int> &temp,vector<vector<int>>&ans,int id,int n){
+    if(id>n){
+        ans.push_back(temp);
+        return;
+    }
+    /*
+    for null printing:
+        if(temp.size()==0){
+            cout<<"{}"
+        }
+        cout<<endl;
+        return;
+    */
+   //picking Vals:
+    temp.push_back(nums[id]);
+    seq(nums,temp,ans,id+1,n);
+    //Backtracking: Not Picking
+    temp.pop_back();
+    seq(nums,temp,ans,id+1,n);
+}
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> ans;
+    vector<int>temp;
+    seq(nums,temp,ans,0,nums.size());
+    return ans;
 }
 int main(){
-    vector<int>v;
+    vector<int>v={3,1,2};
+    subsets(v);
     return 0;
 }
---
+++ medium/subsequence2.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void seq(vector<vector<int>> &ans,vector<int>& temp,vector<int>& nums,int id,int n,int sum){
+    if(sum==0){
+        ans.push_back(temp);
+        return;
+    }
+    if(id>=n || sum<0){
+        return;
+    }
+    if(nums[id]<=sum){
+        temp.push_back(nums[id]);
+        seq(ans,temp,nums,id+1,n,sum-nums[id]);
+        temp.pop_back();
+    }
+    seq(ans,temp,nums,id+1,n,sum);
+}
+vector<vector<int>> subsets(vector<int>& nums,int k) {
+    vector<vector<int>> ans;
+    vector<int>temp;
+    seq(ans,temp,nums,0,nums.size(),k);
+    return ans;
+}
+int main(){
+    vector<int>v={3,1,2,4};
+    vector<vector<int>> ans=subsets(v,5);
+    for(auto &a:ans){
+        for(auto &b:a){
+            cout<<b<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
